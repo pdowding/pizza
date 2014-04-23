523b1cf21f409df2a332db56b10737b0cf8c95dc
Philip Dowding <phil_dowding@hotmail.com>
2014-04-23 14:45:58 -0500

    Add failing test number 2

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -18,4 +18,9 @@ describe Topping do
       expect(topping.name).to eq('olives')
     end
   end
+  it 'sets whether or not the topping is vegetarian' do
+    topping = Topping.new 'bell peppers', vegetarian: true
+
+    expect(topping.vegetarian).to eq(true)
+  end
 end
