1e0e25828a818f6098e021c242a7764c9c21419a
Philip Dowding <phil_dowding@hotmail.com>
2014-04-24 16:15:28 -0500

    Cheese as default topping added

--- pizza.rb
+++ pizza.rb
@@ -1,5 +1,29 @@
 class Pizza
+  attr_accessor :toppings
+
+  def initialize(*toppings)
+    if toppings==[]
+      toppings.push Topping.new("cheese")
+      @toppings=toppings
+    else
+      @toppings=toppings.flatten
+    end
+  end
 end
 
+
+
 class Topping
+  attr_accessor :name, :vegetarian
+  # Instantiate a new topping.
+  #
+  # name - The String name of the topping.
+  #
+  # Returns a new Topping object.
+  def initialize(name, vegetarian: false)
+    @name = name
+    @vegetarian = vegetarian
+  end
+
+
 end
